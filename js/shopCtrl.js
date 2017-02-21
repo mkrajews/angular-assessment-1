0c441edad480a0b64532261a805703ba1e3c6785
mkrajews <mimikrajewski@yahoo.com>
2017-02-21 14:39:30 -0700

    shop page working

--- js/shopCtrl.js
+++ js/shopCtrl.js
@@ -1,3 +1,9 @@
 angular.module('assessment').controller('shopCtrl', function($scope, shopService, $stateParams) {
-
+    $scope.getProducts = function() {
+      shopService.getProductInfo().then(function(response) {
+        console.log('response');
+          $scope.products = response;
+      });
+    };
+    $scope.getProducts();
 });
--- js/shopService.js
+++ js/shopService.js
@@ -1,3 +1,11 @@
 angular.module('assessment').service('shopService', function($http) {
-
+    this.getProductInfo = function() {
+      return $http({
+        method: 'GET',
+        url: 'http://practiceapi.devmounta.in/products'
+      }).then(function(response) {
+          console.log('response ', response);
+          return response.data;
+      });
+    };
 });
